a232736a10413b73b6e52d911a567bddd34860e9
makspll <makspl17@gmail.com>
2020-07-17 13:52:35 +0100

    removed local from git ignore
    install wagtailmetadata

---
+++ personal_site/environments/local.py
@@ -0,0 +1,31 @@
+
+## Wagtail
+INSTALLED_APPS = INSTALLED_APPS + [
+    'wagtail.contrib.forms',
+    'wagtail.contrib.redirects',
+    'wagtail.embeds',
+    'wagtail.sites',
+    'wagtail.users',
+    'wagtail.snippets',
+    'wagtail.documents',
+    'wagtail.images',
+    'wagtail.search',
+    'wagtail.admin',
+    'wagtail.core',
+
+    'modelcluster',
+    'taggit',]
+
+MIDDLEWARE = MIDDLEWARE + ['wagtail.contrib.redirects.middleware.RedirectMiddleware',]
+
+WAGTAIL_SITE_NAME = 'mozolewskimaks.co.uk'
+
+## wagtail-metadata
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'wagtailmetadata',
+]
+
+## custom apps
+
+INSTALLED_APPS = INSTALLED_APPS + ['website']
