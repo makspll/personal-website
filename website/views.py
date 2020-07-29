f5a29b37290e7c53c4c0037034ee600b5ce1a479
makspll <makspl17@gmail.com>
2020-07-29 14:16:13 +0100

    changed view document

--- website/views.py
+++ website/views.py
@@ -11,10 +11,15 @@ def view_document(request, document_id, document_filename):
     
     print( type(response))
     if isinstance(response,HttpResponseRedirect):
+        print(11)
         response = requests.get(response.url)
+        print(22)
         response.headers['Content-Disposition'] = 'inline; filename="{0}"'.format(document_filename)
+        print(33)
     else:
+        print(1)
         # Remove "attachment" from response's Content-Disposition
         response['Content-Disposition'] = 'inline; filename="{0}"'.format(document_filename)
+        print(2)
     # Return the response
     return response
