74ccdefdc90e20b369d83eb554ddca955b163ad6
makspll <makspl17@gmail.com>
2020-07-29 13:45:33 +0100

    changed document veiw

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from wagtail.documents.views import serve
+import requests
 
 
 def view_document(request, document_id, document_filename):
@@ -7,6 +8,11 @@ def view_document(request, document_id, document_filename):
     """
     # Get response from `serve` first
     response = serve.serve(request, document_id, document_filename)
+
+    redirect_url = response.get("Location")
+    
+    if redirect_url:
+        response = requests.get(redirect_url)
 
     # Remove "attachment" from response's Content-Disposition
     contdisp = response.get('Content-Disposition',"")
