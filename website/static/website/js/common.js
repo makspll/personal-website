9c155070f186fadcdf3dbc8d20da79ba61ff0da3
makspll <makspl17@gmail.com>
2020-08-01 03:08:44 +0100

    added embed css

--- website/static/website/js/common.js
+++ website/static/website/js/common.js
@@ -1,7 +1,6 @@
 console.log("hello")
 $(document).ready(function(){
     $(".collapse-arrow-container").click(function(){
-        console.log("hello")
         $(this).find(".collapse-arrow").toggleClass("active")
     });
 });
