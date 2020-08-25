f92a490c7891ceb3d186adc15eda87c0efe57e68
makspll <makspl17@gmail.com>
2020-08-25 19:48:54 +0100

    fixed error

--- client/index.js
+++ client/index.js
@@ -1,6 +1,16 @@
+
+import FreeformListingBlock from './components/FreeformListingBlock';
+import ArticleListingBlock from './components/ArticleListingBlock';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AOS from 'aos';
+import Prism from 'prismjs';
+import Rellax from 'rellax';
+import 'popper.js';
+import 'bootstrap';
+
 import './css/all.scss';
 import './js/common.js';
-require.include("./vendor");
 var images = document.getElementsByClassName('img-background');
 
 var rellax = new Rellax('.rellax', {
